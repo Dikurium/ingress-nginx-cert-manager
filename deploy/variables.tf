12e128a0db6e69c5cc8407d03a966242fbb614f6
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2022-11-10 13:37:53 +0100

    fix: :bug: add missing variables for cluster

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -14,6 +14,18 @@ variable "domain" {
   type        = string
 }
 
+variable "dikurium_k8s_cluster_name_all" {
+  description = "The name of the Dikurium main Kubernetes cluster"
+  default     = "k8s-dik-all"
+  type        = string
+}
+
+variable "dikurium_k8s_pool_name" {
+  description = "The name of the Dikurium main Kubernetes pool"
+  default     = "pool-dik-all"
+  type        = string
+}
+
 variable "cert_manager_additional_resources" {
   description = "Additional resources to add to the Cert-Manager deployment, such as a ClusterIssuer"
   type        = list(string)
